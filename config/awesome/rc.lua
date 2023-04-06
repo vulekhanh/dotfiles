68def77fa16130e970cd83ddc44b1274dfd04c8f
vulekhanh <61068720+vulekhanh@users.noreply.github.com>
2023-04-06 21:41:11 +0700

    added theme and background for awesome, changed window layout

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -10,6 +10,8 @@ require("awful.autofocus")
 local wibox = require("wibox")
 -- Theme handling library
 local beautiful = require("beautiful")
+beautiful.init("/home/vulekhanh/.config/awesome/theme.lua")
+beautiful.get().wallpaper = "/home/vulekhanh/.dotfiles/wallpapers/sci-fi.jpg"
 -- Notification library
 local naughty = require("naughty")
 local menubar = require("menubar")
@@ -48,7 +50,7 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "kitty"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
@@ -61,22 +63,22 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.floating,
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
-    awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
+  --awful.layout.suit.floating,
+  --awful.layout.suit.tile,
+  --awful.layout.suit.tile.left,
+  --awful.layout.suit.tile.bottom,
+  --awful.layout.suit.tile.top,
+  --awful.layout.suit.fair,
+  --awful.layout.suit.fair.horizontal,
+  --awful.layout.suit.spiral.dwindle,
+  --awful.layout.suit.max,
+  --awful.layout.suit.max.fullscreen,
+  --awful.layout.suit.magnifier,
+  --awful.layout.suit.corner.nw,
+  -- awful.layout.suit.corner.ne,
+  -- awful.layout.suit.corner.sw,
+  -- awful.layout.suit.corner.se,
 }
 -- }}}
 
