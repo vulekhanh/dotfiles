982d95f3bb187a4ded980cc32b0208903b81a5d0
Khanh Le Vu <61068720+vulekhanh@users.noreply.github.com>
2023-04-06 23:02:21 +0700

    added yay installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo pacman -S yay
 yay -S oh-my-posh-bin
 rm ~/.zshrc
 ln -s ~/.dotfiles/config/zsh/.zshrc ~/
