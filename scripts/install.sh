71c41cc0bae6c096afc68dcaa28760e9e3b3738b
Hamza12700 <c44help@gmail.com>
2023-04-23 14:45:04 +0000

    fix a small typo

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@ configDir='../config'
 # intall oh my zsh and plugins for zsh
 echo "Installing OH-MY-ZSH\n"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "\nInstalling Plugin\ns"
+echo "\nInstalling Plugins\n"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
