53540d5eaa5ff0115a3bbe2099cb37c5e06d2d03
Hamza12700 <c44help@gmail.com>
2023-04-23 15:16:06 +0000

    fix uninstall script

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,1 +1,21 @@
-yay -Rns kcalc yakuake elisa konsole 
+# Uninstalling OH-MY-ZSH
+echo "Uninstalling OH-MY-ZSH\n"
+uninstall_oh_my_zsh
+
+#Removing .zshrc link
+echo "Removing .zshrc symbolic link"
+unlink ~/.zshrc
+if [ -e ~/.zshrc.bak ]; then
+  echo "Switching to default .zshrc"
+  mv ~/.zshrc.bak ~/.zshrc
+else
+  echo "No .zshrc backup found!"
+
+#Removing neofetch symbolic link
+echo "\nRemoving neofetch symbolic link\n"
+unlink ~/.config/neofetch/config.conf
+if [ -e ~/.config/neofetch/config.conf.bak ]; then
+  echo "Switching to default neofetch config"
+  mv ~/.config/neofetch/config.conf.bak ~/.config/neofetch/config.conf
+else
+  echo "No neofetch backup found!"
