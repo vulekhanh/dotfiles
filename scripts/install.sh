416c1c0616996240ba385938a7ed15c28b133e82
vulekhanh <61068720+vulekhanh@users.noreply.github.com>
2023-04-23 13:57:13 +0700

    removed lunarvim installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,42 +5,11 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 # install neovim, nvm and yay package manager for managing AUR packages
 sudo pacman -S neovim python-pynvim nvm yay
-yay -S oh-my-posh-bin awesome-git picom-git rofi
+yay -S oh-my-posh-bin awesome-git picom-git rofi github-cli
 # create symbolic links for .zshrc, .gitconifg
-rm ~/.zshrc && ln -s ~/.dotfiles/config/zsh/.zshrc ~/ && ln -s ~/.dotfiles/git/.gitconfig ~/
-exec zsh
-
-# install nodejs 
-nvm install node
-
-# install cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-
-# install kitty terminal
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-
-# You can find the below scripts from Kitty main website.
-# Create symbolic links to add kitty and kitten to PATH
-ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
-# Place the kitty.desktop file somewhere it can be found by the OS
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-# Open text files and images in kitty via Dolphin file manager also add the kitty-open.desktop file
-cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-# Update the paths to the kitty and its icon in the kitty.desktop file(s)
-sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
-
-# create symbolic link for kitty config
-rm .config/kitty/kitty.conf && ln -s ~/.dotfiles/config/kitty/kitty.conf ~/.config/kitty/
-
-# create symlinks for awesome, picom, rofi
-ln -s ~/.dotfiles/config/awesome/rc.lua ~/.config/awesome/
-ln -s ~/.dotfiles/config/awesome/theme-personal.lua ~/.config/awesome/themes/multicolor/
-ln -s ~/.dotfiles/config/awesome/icons ~/.config/awesome/themes/multicolor/
-ln -s ~/.dotfiles/config/picom/picom.conf ~/.config/picom/
-ln -s ~/.dotfiles/config/rofi/config.rasi ~/.config/rofi/
+rm ~/.zshrc && ln -s ~/.dotfiles/config/zsh/.zshrc ~/.zshrc
 
 # create symlink for neofetch
 rm ~/.config/neofetch/config.conf && ln ~/.dotfiles/config/neofetch/config.conf ~/.config/neofetch/
 
+exec zsh
