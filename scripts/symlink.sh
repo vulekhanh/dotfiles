4740cd77af9ed9812d929e2d49df36e26c954941
Hamza12700 <c44help@gmail.com>
2023-04-23 12:39:31 +0000

    improve symlinks script

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,8 +1,17 @@
+configDir='../config'
+
 # create symbolic link for kitty config
-rm .config/kitty/kitty.conf && ln -s ~/.dotfiles/config/kitty/kitty.conf ~/.config/kitty/
+if [ -e ~/.config/kitty/kitty.conf ]; then
+  echo "Kitty config found"
+  echo "Moving kitty.conf to kitty.conf.bak"
+  mv ~/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf.bak
+  ln -s ~/.dotfiles/config/kitty/kitty.conf ~/.config/kitty/
+else
+  ln -s ~/.dotfiles/config/kitty/kitty.conf ~/.config/kitty/
+fi
 # create symlinks for awesome, picom
 sudo rm -r ~/.config/awesome/themes/multicolor/icons
-ln -s ~/.dotfiles/config/awesome/rc.lua ~/.config/awesome/
-ln -s ~/.dotfiles/config/awesome/theme-personal.lua ~/.config/awesome/themes/multicolor/
-ln -s ~/.dotfiles/config/awesome/icons ~/.config/awesome/themes/multicolor/
-ln -s ~/.dotfiles/config/picom/picom.conf ~/.config/picom/
+ln -s $configDir/awesome/rc.lua ~/.config/awesome/
+ln -s $configDir/awesome/theme-personal.lua ~/.config/awesome/themes/multicolor/
+ln -s $configDir/awesome/icons ~/.config/awesome/themes/multicolor/
+ln -s $configDir/picom/picom.conf ~/.config/picom/
