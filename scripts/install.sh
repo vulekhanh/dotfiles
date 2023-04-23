08f1e4f1631151969d77d7218e689ea976b7b81d
Hamza12700 <c44help@gmail.com>
2023-04-23 12:39:18 +0000

    improve install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,33 @@
+configDir='../config'
+
 # intall oh my zsh and plugins for zsh
+echo "Installing OH-MY-ZSH\n"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "\nInstalling Plugin\ns"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # install neovim, nvm and yay package manager for managing AUR packages
-sudo pacman -S neovim python-pynvim nvm yay
+echo "Installing Packages"
+sudo pacman -Syu
+sudo pacman -S neovim python-pynvim nodejs npm yay
 yay -S oh-my-posh-bin awesome-git picom-git rofi github-cli
 # create symbolic links for .zshrc, .gitconifg
-rm ~/.zshrc && ln -s ~/.dotfiles/config/zsh/.zshrc ~/.zshrc
+if [ -e ~/.zshrc ]; then
+  echo "ZSH config found"
+  echo "Moveing .zshrc to .zshrc.bak"
+  mv ~/.zshrc ~/.zshrc.bak
+  ln -s $configDir/zsh/.zshrc ~/.zshrc
+else
+  ln -s $configDir/zsh/.zshrc ~/.zshrc
+fi
 
 # create symlink for neofetch
-rm ~/.config/neofetch/config.conf && ln ~/.dotfiles/config/neofetch/config.conf ~/.config/neofetch/
-
-exec zsh
+if [ -e ~/.config/neofetch/config.conf ]; then
+  echo "Neofetch config found"
+  echo "Moveing neofetch/config.conf to neofetch/config.conf.bak"
+  mv ~/.config/neofetch/config.conf ~/.config/neofetch/config.conf.bak
+  ln $configDir/neofetch/config.conf ~/.config/neofetch/
+else
+  ln $configDir/neofetch/config.conf ~/.config/neofetch/
+fi
