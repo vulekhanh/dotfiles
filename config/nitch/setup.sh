9e8e6c3d9a44482b3c5848d567e5d7bc5f39def5
Hamza12700 <c44help@gmail.com>
2023-04-24 11:41:13 +0000

    No need to download the nitch binary
    Because the custom nitch config we need to build it from source

--- config/nitch/setup.sh
+++ config/nitch/setup.sh
@@ -1,24 +1,5 @@
 #!/bin/sh
 
-linkNerd="https://github.com/unxsh/nitch/releases/download/0.1.6/nitchNerd"
-linkNoNerd="https://github.com/unxsh/nitch/releases/download/0.1.6/nitchNoNerd"
-
 echo "Uninstalling nitch\n"
 yay -Rs nitch --noconfirm
 
-read -p "\nUse nerd font symbols? (y/n): " symbolsYN
-echo "Installation..."
-
-case $symbolsYN in
-  "y")
-    wget $linkNerd
-    chmod +x nitchNerd
-    sudo mv nitchNerd /usr/local/bin/nitch
-  ;;
-
-  "n")
-    wget $linkNoNerd
-    chmod +x nitchNoNerd
-    sudo mv nitchNoNerd /usr/local/bin/nitch
-  ;;
-esac
