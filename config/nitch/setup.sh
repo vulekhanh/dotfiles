060c3ec9c41ee0639e3e070de48e24b983a319e6
Hamza12700 <c44help@gmail.com>
2023-04-24 12:10:03 +0000

    Building nitch from source

--- config/nitch/setup.sh
+++ config/nitch/setup.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Uninstalling nitch\n"
 yay -Rs nitch --noconfirm
 
+echo "Building Nitch\n"
+nimble build
+clear
+echo "Linking the binary to /usr/local/bin/"
+ls -s nitch /usr/local/bin/
