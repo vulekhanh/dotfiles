dc6a245a4e74d38d730369c6ea216d131e9ead41
Khanh Le Vu <61068720+vulekhanh@users.noreply.github.com>
2023-04-26 21:26:41 +0700

    removed fira-code installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,27 +5,14 @@ echo "Installing OH-MY-ZSH\n"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "\nInstalling Plugins\n"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 
 # install neovim, nvm and yay package manager for managing AUR packages
 echo "Installing Packages"
 sudo pacman -Syu
-sudo pacman -S neovim python-pynvim nodejs npm yay kitty exa bat unzip nim --needed
+sudo pacman -S ttf-firacode-nerd neovim python-pynvim nodejs npm yay exa bat unzip nim --needed
 yay -S oh-my-posh-bin awesome-git picom-git rofi github-cli
 
-#Installing Fira Code Font
-echo "\nInstalling Fira Code Font\n"
-unzip $configDir/fonts/Fira\ Code\ .zip
-if [ -e ~/.local/share/fonts/ ]; then
-  mv *.ttf ~/.local/share/fonts/
-  fc-cache -vf
-else
-  echo "Fonts directory don't exist"
-  echo "Creating fonts directory in ~/.local/share/fonts"
-  mkdir ~/.local/share/fonts
-  mv *.ttf ~/.local/share/fonts/
-  fc-cache -vf
-fi
 
 # create symbolic links for .zshrc, .gitconifg
 if [ -e ~/.zshrc ]; then
