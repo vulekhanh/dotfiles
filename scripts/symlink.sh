9a275aab7e8f1eec7fbbf51bb2a38f2f5c84b48c
vulekhanh <61068720+vulekhanh@users.noreply.github.com>
2023-04-29 03:13:39 +0700

    moved create symlinks script to symlink.sh

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -25,3 +25,42 @@ fi
 ln -s $configDir/awesome/theme-personal.lua ~/.config/awesome/themes/multicolor/
 ln -s $configDir/awesome/icons ~/.config/awesome/themes/multicolor/
 ln -s $configDir/picom/picom.conf ~/.config/picom/
+
+
+# create symbolic links for .zshrc, .gitconifg
+if [ -e ~/.zshrc ]; then
+  echo "ZSH config found"
+  echo "Moveing .zshrc to .zshrc.bak"
+  mv ~/.zshrc ~/.zshrc.bak
+  ln -s $configDir/zsh/.zshrc ~/.zshrc
+else
+  ln -s $configDir/zsh/.zshrc ~/.zshrc
+fi
+
+# create symlink for neofetch
+if [ -e ~/.config/neofetch/config.conf ]; then
+  echo "Neofetch config found"
+  echo "Moveing neofetch/config.conf to neofetch/config.conf.bak"
+  mv ~/.config/neofetch/config.conf ~/.config/neofetch/config.conf.bak
+  ln $configDir/neofetch/config.conf ~/.config/neofetch/
+else
+  ln $configDir/neofetch/config.conf ~/.config/neofetch/
+fi
+
+# Rofi config
+if [ -e ~/.config/rofi/ ]; then
+  echo "Rofi config directory found"
+  if [ -e ~/.config/rofi/config.rasi ]; then
+    echo "Rofi config found"
+    echo "Moving config.rasi to config.rasi.bak"
+    mv ~/.config/rofi/config.rasi ~/.config/rofi/config.rasi.bak
+    ln -s $configDir/rofi/config/* ~/.config/rofi/
+  else
+    mv ~/.config/rofi/config.rasi ~/.config/rofi/config.rasi.bak
+    ln -s $configDir/rofi/config/* ~/.config/rofi/
+  fi
+else
+  echo "Creating rofi config directory"
+  mkdir ~/.config/rofi
+  ln -s $configDir/rofi/config/* ~/.config/rofi/
+fi
