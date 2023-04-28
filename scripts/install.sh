78f2ba3f70d9c20e4fd470fa55fcfc6a1792fcaf
vulekhanh <61068720+vulekhanh@users.noreply.github.com>
2023-04-28 23:34:23 +0700

    added packages for taking screenshot and clipboard manager, including maim, xdotool, xclip; qimgv for viewing image

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,7 +14,7 @@ echo "Installing Packages"
 sudo pacman -Syu
 
 # install fonts for rendering glyphs, dependencies for lunarVim, git utilities, bluetooth, audio
-sudo pacman -S ttf-firacode-nerd ttf-droid git pacman-contrib python-pynvim nodejs npm exa bat unzip lazygit nim alsa-utils alsa-firmware pipewire-audio pipewire-alsa pipewire-pulse bluez lxappearance --needed
+sudo pacman -S ttf-firacode-nerd ttf-droid git pacman-contrib python-pynvim nodejs npm exa bat unzip lazygit nim alsa-utils alsa-firmware pipewire-audio pipewire-alsa pipewire-pulse bluez lxappearance maim xdotool xclip qimgv --needed
 
 # Yay installation
 if which yay >/dev/null; then
