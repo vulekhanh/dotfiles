7700f5b54d002f43e5e0eebd869c1c67785adbe9
vulekhanh <61068720+vulekhanh@users.noreply.github.com>
2023-04-30 19:58:32 +0700

    removed freedesktop to avoid lagging while using awesomewm

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -17,7 +17,6 @@ local wibox         = require("wibox")
 local beautiful     = require("beautiful")
 local naughty       = require("naughty")
 local lain          = require("lain")
-local freedesktop   = require("freedesktop")
 local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
 local mytable = awful.util.table or gears.table -- 4.{0,1} compatibility
@@ -60,13 +59,13 @@ end
 -- {{{ Autostart windowless processes
 
 -- This function will run once every time Awesome is started
-local function run_once(cmd_arr)
-  for _, cmd in ipairs(cmd_arr) do
-    awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
-  end
-end
-
-run_once({ "urxvtd", "unclutter -root" }) -- comma-separated entries
+--local function run_once(cmd_arr)
+--  for _, cmd in ipairs(cmd_arr) do
+--    awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
+--  end
+--end
+--
+--run_once({ "urxvtd", "unclutter -root" }) -- comma-separated entries
 
 -- This function implements the XDG autostart specification
 --[[
@@ -176,16 +175,6 @@ local myawesomemenu = {
   { "Quit",        function() awesome.quit() end },
 }
 
-awful.util.mymainmenu = freedesktop.menu.build {
-  before = {
-    { "Awesome", myawesomemenu, beautiful.awesome_icon },
-    -- other triads can be put here
-  },
-  after = {
-    { "Open terminal", terminal },
-    -- other triads can be put here
-  }
-}
 
 -- Set the Menubar terminal for applications that require it
 --menubar.utils.terminal = terminal
@@ -216,7 +205,6 @@ awful.screen.connect_for_each_screen(function(s) beautiful.at_screen_connect(s) 
 -- {{{ Mouse bindings
 
 root.buttons(mytable.join(
-  awful.button({}, 3, function() awful.util.mymainmenu:toggle() end),
   awful.button({}, 4, awful.tag.viewnext),
   awful.button({}, 5, awful.tag.viewprev)
 ))
@@ -265,10 +253,6 @@ globalkeys = mytable.join(
       if client.focus then client.focus:raise() end
     end,
     { description = "focus right", group = "client" }),
-
-  -- Menu
-  awful.key({ modkey, }, "w", function() awful.util.mymainmenu:show() end,
-    { description = "show main menu", group = "awesome" }),
 
   -- Layout manipulation
   awful.key({ modkey, }, "Right", function() awful.tag.incmwfact(0.05) end,
