2cd8d93c423ffeb9dcf0508a138c9b7588045a00
vulekhanh <61068720+vulekhanh@users.noreply.github.com>
2023-05-27 21:13:18 +0700

    added bufferline configuration

---
+++ config/nvim/lua/user/bufferline.lua
@@ -0,0 +1,9 @@
+local status_ok, bufferline = pcall(require, "bufferline")
+if not status_ok then
+  return
+end
+bufferline.setup {
+  options = {
+    offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+  }
+}
