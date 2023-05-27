324a3ebb1065fa925095484a242219452bbf4f80
vulekhanh <61068720+vulekhanh@users.noreply.github.com>
2023-05-27 19:58:57 +0700

    set flavour to frappe and added transparent background to neovim

--- config/nvim/lua/user/colorscheme.lua
+++ config/nvim/lua/user/colorscheme.lua
@@ -1,7 +1,9 @@
+require("catppuccin").setup({
+  flavour = "frappe",
+  transparent_background = true,
+})
 local colorscheme = "catppuccin"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-  vim.api.nvim_set_hl(0,"Normal", {bg = "none"})
-  vim.api.nvim_set_hl(0,"NormalFloat", {bg = "none"})
 if not status_ok then
   return
 end
