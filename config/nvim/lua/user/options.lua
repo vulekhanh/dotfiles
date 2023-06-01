81bd49863ece6e50876ff7533464826b5f386eb2
vulekhanh <61068720+vulekhanh@users.noreply.github.com>
2023-06-01 19:01:46 +0700

    enable folding in neovim

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -36,6 +36,8 @@ local options = {
   sidescrolloff = 8,                       -- minimal number of screen columns either side of cursor if wrap is `false`
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
   whichwrap = "bs<>[]hl",                  -- which "horizontal" keys are allowed to travel to prev/next line
+  foldmethod = "expr",                     -- set folding method
+  foldexpr = "nvim_treesitter#foldexpr()",
 }
 
 for k, v in pairs(options) do
