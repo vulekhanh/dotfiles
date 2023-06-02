8c26e365416dd5335c8b67bdb1bdbc78841bd2cb
vulekhanh <61068720+vulekhanh@users.noreply.github.com>
2023-06-02 13:35:08 +0700

    set up lsp for neovim

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -65,4 +65,25 @@ require("lazy").setup({
     -- Project management for neovim
   { "ahmedkhalf/project.nvim" },
 
+    -- Lsp
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+        'williamboman/mason.nvim',
+        build = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  },
 })
