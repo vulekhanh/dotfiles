fc6055d5d42e99127d091dabc2eabc0baecc8dd3
vulekhanh <61068720+vulekhanh@users.noreply.github.com>
2023-06-03 16:29:44 +0700

    binded new keybinds for navigating buffers
    Ctrl + f to move forward, Ctrl + b to move backward

--- config/nvim/lua/user/cmp.lua
+++ config/nvim/lua/user/cmp.lua
@@ -1,5 +1,4 @@
 local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
 require("luasnip.loaders.from_vscode").lazy_load()
 
 
@@ -62,11 +61,7 @@ cmp.setup({
     -- Ctrl+Space to trigger completion menu
     ['<C-Space>'] = cmp.mapping.complete(),
 
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<C-f>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
@@ -80,7 +75,7 @@ cmp.setup({
       end
     end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    ["<C-b>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
