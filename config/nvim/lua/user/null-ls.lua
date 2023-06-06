f402ebe295e37b94e37777cb61958df1254cc237
vulekhanh <61068720+vulekhanh@users.noreply.github.com>
2023-06-06 13:06:18 +0700

    null-ls code actions with gitsigns

--- config/nvim/lua/user/null-ls.lua
+++ config/nvim/lua/user/null-ls.lua
@@ -1,6 +1,7 @@
 local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 local completion = null_ls.builtins.completion
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
@@ -12,6 +13,7 @@ null_ls.setup({
 		completion.luasnip,
 		completion.spell,
 		completion.tags,
+		code_actions.gitsigns,
 	},
 	-- Format on save
 	on_attach = function(current_client, bufnr)
