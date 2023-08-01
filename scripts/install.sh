0179917374d604a86c5fe32d998cfe23c7e1015b
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-08-01 05:42:55 -0700

    made the install script more fancy

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,39 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
-configDir='../config'
+greenColor="#32a852"
 
-# intall oh my zsh and plugins for zsh
-echo "Installing OH-MY-ZSH\n"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "\nInstalling Plugins\n"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+gum style --foreground 212 --margin "1 2" --padding "1 2" --bold --border thick --border-foreground 215 --align center "Welcome To The Installer"
 
-# install neovim, yay package manager for managing AUR packages
-echo "Installing Packages"
-sudo pacman -Syu
+# Installing OH-MY-ZSH
+gum spin --spinner meter --show-output --title "Installing OH-MY-ZSH" -- sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# install fonts for rendering glyphs, dependencies for lunarVim, git utilities, bluetooth, audio
-sudo pacman -S ttf-droid ttf-firacode-nerd git pacman-contrib neovim python-pynvim nodejs npm exa bat unzip lazygit alsa-utils alsa-firmware bluez bluez-utils lxappearance maim xdotool xclip pulseaudio pulseaudio-alsa pulseaudio-bluetooth arandr brightnessctl zoxide iw ttf-font-awesome docker kitty mariadb --needed
+# Installing OH-MY-ZSH Plugins
+gum spin --spinner line --title "Installing OH-MY-ZSH Plugin:zsh-syntax-highlighting" -- git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+gum spin --spinner line --title "Installing OH-MY-ZSH Plugin:zsh-autosuggestions" -- git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# Yay installation
+# Updating System
+gum style --foreground=$greenColor --margin "1 2" "Updating System"
+sudo pacman -Syu --noconfirm
+
+# Installing Packages
+
+yayInstallation() {
+  git clone https://aur.archlinux.org/yay.git
+  pushd yay/
+  makepkg -si --noconfirm
+  popd
+}
+
+gum style --foreground=$margin --margin "1 2" "Installing Required Packages"
 if which yay >/dev/null; then
-  yay -S oh-my-posh-bin awesome-git picom-git rofi github-cli ranger qimgv p7zip lazydocker microsoft-edge-stable-bin --needed
+  yay -S oh-my-posh-bin awesome-git picom-git rofi github-cli ranger qimgv p7zip lazydocker microsoft-edge-stable-bin ttf-droid ttf-firacode-nerd git pacman-contrib neovim python-pynvim nodejs npm exa bat unzip lazygit alsa-utils alsa-firmware bluez bluez-utils lxappearance maim xdotool xclip pulseaudio pulseaudio-alsa pulseaudio-bluetooth arandr brightnessctl zoxide iw ttf-font-awesome docker kitty mariadb --needed
 else
-  echo "yay not found!"
-  read -p "Do you want to build yay from source? [y/N] " yn
-  case "$yn" in
-    y* ) 
-      git clone https://aur.archlinux.org/yay.git
-      pushd yay/
-      makepkg -si --noconfirm
-      popd
-      installer
-    ;;
-    *) 
-      echo "Yay is not installed!"
-      echo "Existing"
-      exit
-    ;;
-  esac
+  gum style --foreground "#eb4034" --margin "1 2" --bold "yay not found!"
+  gum confirm "Would you like to install it?" && yayInstallation || gum style --foreground "#eb4034" --margin "1 2" --bold "Yay isn't installed!"
 fi
