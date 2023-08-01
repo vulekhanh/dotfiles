2623bb97fcd4581fe16bfc8fb4ab063cf3117d38
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-08-01 06:01:11 -0700

    made `uninstall.sh` fancy

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,21 +1,26 @@
 # Uninstalling OH-MY-ZSH
-echo "Uninstalling OH-MY-ZSH\n"
+
+printG() {
+  gum style --foreground 215 --margin "1 2" "$1"
+}
+
+printG "Uninstalling OH-MY-ZSH\n"
 uninstall_oh_my_zsh
 
 #Removing .zshrc link
-echo "Removing .zshrc symbolic link"
+printG "Removing .zshrc symbolic link"
 unlink ~/.zshrc
 if [ -e ~/.zshrc.bak ]; then
-  echo "Switching to default .zshrc"
+  printG "Switching to default .zshrc"
   mv ~/.zshrc.bak ~/.zshrc
 else
-  echo "No .zshrc backup found!"
+  printG "No .zshrc backup found!"
 
 #Removing neofetch symbolic link
-echo "\nRemoving neofetch symbolic link\n"
+printG "\nRemoving neofetch symbolic link\n"
 unlink ~/.config/neofetch/config.conf
 if [ -e ~/.config/neofetch/config.conf.bak ]; then
-  echo "Switching to default neofetch config"
+  printG "Switching to default neofetch config"
   mv ~/.config/neofetch/config.conf.bak ~/.config/neofetch/config.conf
 else
-  echo "No neofetch backup found!"
+  printG "No neofetch backup found!"
