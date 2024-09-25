1da6e622555afaa1654bf1d707d600fd19ebcb89
vulekhanh <vulekhanh69@gmail.com>
2024-09-25 11:23:22 -0500

    removed nvim config to fix conflicted plugins

--- config/nvim/lua/user/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-require("catppuccin").setup({
-	flavour = "macchiato",
-	transparent_background = true,
-})
-local colorscheme = "catppuccin"
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-	return
-end
