9a995c44637783e065319c7a59122f32a9484b3e
vulekhanh <vulekhanh69@gmail.com>
2024-09-28 22:49:55 -0500

    installed treesitter for neovim and configured auto tag

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -1,14 +1,7 @@
-require("lazy").setup({{
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "python" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
- }})
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+  ensure_installed = { "cpp", "lua", "vim", "query", "javascript", "html", "css", "python" },
+  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },  
+  })
