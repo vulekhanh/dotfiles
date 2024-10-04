f2092a5b21fbd845450e0f3c517c930379c2bebd
vulekhanh <vulekhanh69@gmail.com>
2024-10-03 23:25:24 -0500

    added debug adapter, and dap-ui

---
+++ config/nvim/lua/config/dap.lua
@@ -0,0 +1,50 @@
+local dap, dapui = require("dap"), require("dapui")
+dapui.setup()
+dap.listeners.before.attach.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+	dapui.close()
+end
+
+--c++ configuration
+local dap = require("dap")
+dap.adapters.cppdbg = {
+	id = "cppdbg",
+	type = "executable",
+	command = "/home/vulekhanh/.vscode/extensions/ms-vscode.cpptools-1.22.7-linux-x64/debugAdapters/bin/OpenDebugAD7",
+}
+dap.configurations.cpp = {
+	{
+		name = "Launch file",
+		type = "cppdbg",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopAtEntry = true,
+	},
+	{
+		name = "Attach to gdbserver :1234",
+		type = "cppdbg",
+		request = "launch",
+		MIMode = "gdb",
+		miDebuggerServerAddress = "localhost:1234",
+		miDebuggerPath = "/usr/bin/gdb",
+		cwd = "${workspaceFolder}",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+	},
+}
+dap.defaults.fallback.external_terminal = {
+	command = "/usr/bin/kitty",
+	args = { "-e" },
+}
--- config/nvim/lua/config/nvim-cmp.lua
+++ config/nvim/lua/config/nvim-cmp.lua
@@ -1,26 +1,30 @@
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
-local cmp_format = require('lsp-zero').cmp_format({details = true})
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
+local cmp_format = require("lsp-zero").cmp_format({ details = true })
 require("luasnip.loaders.from_vscode").lazy_load()
 cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-    {name = 'luasnip'},
-  },
-  window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-  },
-  snippet = {
-    expand = function(args)
-      -- Need Neovim v0.10 to use vim.snippet
-      vim.snippet.expand(args.body)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-    ['<Tab>'] = cmp.mapping.confirm({select = false}),
-  }),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	snippet = {
+		expand = function(args)
+			-- Need Neovim v0.10 to use vim.snippet
+			vim.snippet.expand(args.body)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	preselect = "item",
+	completion = {
+		completeopt = "menu,menuone,noinsert",
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<Tab>"] = cmp_action.luasnip_supertab(),
+		["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	}),
 })
